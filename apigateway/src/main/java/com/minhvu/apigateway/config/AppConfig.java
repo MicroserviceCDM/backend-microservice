4c62bc14139ec0db356f4bb36bd7642a6c5df3c8
tuilakhanh <bacdau852@gmail.com>
2024-12-29 04:39:53 +0700

    fix

--- apigateway/src/main/java/com/minhvu/apigateway/config/AppConfig.java
+++ apigateway/src/main/java/com/minhvu/apigateway/config/AppConfig.java
@@ -7,7 +7,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.util.pattern.PathPatternParser;
-import java.util.Collections;
+import java.util.Arrays;
 
 @Configuration
 public class AppConfig {
