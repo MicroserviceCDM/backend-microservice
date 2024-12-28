e3ca181a1045329ae1db299eb05e87c661090296
tuilakhanh <bacdau852@gmail.com>
2024-12-29 03:08:51 +0700

    debug respose

--- authservice/src/main/java/com/minhvu/authservice/filter/RequestLoggingFilter.java
+++ authservice/src/main/java/com/minhvu/authservice/filter/RequestLoggingFilter.java
@@ -2,10 +2,13 @@ package com.minhvu.authservice.filter;
 
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.web.util.ContentCachingResponseWrapper;
 
 import java.io.IOException;
+import java.util.Collection;
 import java.util.Collections;
 
 @Component
@@ -17,12 +20,20 @@ public class RequestLoggingFilter implements Filter {
             throws IOException, ServletException {
         
         HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
         
+        // Log Request Headers
         log.info("Request Headers for {}: ", httpRequest.getRequestURI());
         Collections.list(httpRequest.getHeaderNames())
                 .forEach(headerName -> 
-                    log.info("{}: {}", headerName, httpRequest.getHeader(headerName)));
+                    log.info("Request {}: {}", headerName, httpRequest.getHeader(headerName)));
 
         chain.doFilter(request, response);
+
+        // Log Response Headers
+        log.info("Response Headers for {}: ", httpRequest.getRequestURI());
+        httpResponse.getHeaderNames()
+                .forEach(headerName ->
+                    log.info("Response {}: {}", headerName, httpResponse.getHeader(headerName)));
     }
 }
