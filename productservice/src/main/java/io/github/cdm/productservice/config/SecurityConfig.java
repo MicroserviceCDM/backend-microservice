6d4494469ae860b0dc42ce17eaeb9c931f7c7831
tuilakhanh <bacdau852@gmail.com>
2024-12-29 14:40:00 +0700

    test

--- productservice/src/main/java/io/github/cdm/productservice/config/SecurityConfig.java
+++ productservice/src/main/java/io/github/cdm/productservice/config/SecurityConfig.java
@@ -1,33 +1,33 @@
-package io.github.cdm.productservice.config;
+// package io.github.cdm.productservice.config;
 
-import lombok.AllArgsConstructor;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.CorsWebFilter;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
-import org.springframework.web.util.pattern.PathPatternParser;
+// import lombok.AllArgsConstructor;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.security.authentication.AuthenticationProvider;
+// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+// import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+// import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+// import org.springframework.security.web.SecurityFilterChain;
+// import org.springframework.web.cors.CorsConfiguration;
+// import org.springframework.web.cors.reactive.CorsWebFilter;
+// import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+// import org.springframework.web.util.pattern.PathPatternParser;
 
-import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+// import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
-@Configuration
-@EnableWebSecurity
-@AllArgsConstructor
-public class SecurityConfig {
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http
-                .csrf(AbstractHttpConfigurer::disable)
-                .cors(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth -> {
-                    auth.requestMatchers("/api/v1/products/**").authenticated()
-                            .anyRequest().permitAll();
-                })
-                .build();
-    }
-}
+// @Configuration
+// @EnableWebSecurity
+// @AllArgsConstructor
+// public class SecurityConfig {
+//     @Bean
+//     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//         return http
+//                 .csrf(AbstractHttpConfigurer::disable)
+//                 .cors(AbstractHttpConfigurer::disable)
+//                 .authorizeHttpRequests(auth -> {
+//                     auth.requestMatchers("/api/v1/products/**").authenticated()
+//                             .anyRequest().permitAll();
+//                 })
+//                 .build();
+//     }
+// }
