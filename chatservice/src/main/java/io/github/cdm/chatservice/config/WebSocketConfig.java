ad05829008aecc7d44431dd331d0f3c1905ca13c
tuilakhanh <bacdau852@gmail.com>
2024-12-29 07:33:26 +0700

    fix rule

--- chatservice/src/main/java/io/github/cdm/chatservice/config/WebSocketConfig.java
+++ chatservice/src/main/java/io/github/cdm/chatservice/config/WebSocketConfig.java
@@ -11,14 +11,14 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/api/chatws/ws")
+        registry.addEndpoint("/ws")
                 .setAllowedOriginPatterns("*")
                 .withSockJS();
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.setApplicationDestinationPrefixes("/api/chatws/app");
+        registry.setApplicationDestinationPrefixes("/app");
         registry.enableSimpleBroker("/chatroom", "/user");
         registry.setUserDestinationPrefix("/user");
     }
