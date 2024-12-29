ab3104d931ddfff3a6bb81be34860318ace1aaf7
tuilakhanh <bacdau852@gmail.com>
2024-12-29 14:03:23 +0700

    test

--- productservice/src/main/java/io/github/cdm/productservice/config/SecurityConfig.java
+++ productservice/src/main/java/io/github/cdm/productservice/config/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> {
-                    auth.requestMatchers("/**").authenticated()
+                    auth.requestMatchers("").authenticated()
                             .anyRequest().permitAll();
                 })
                 .build();
